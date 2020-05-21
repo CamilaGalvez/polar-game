92a46414a4c6d1e721eee33d3a2e54a7b8cff72c
CamilaGalvez <64030473+CamilaGalvez@users.noreply.github.com>
2020-05-21 13:35:55 -0500

    Add files via upload

---
+++ tiled/tilesito.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tilesito" tilewidth="47" tileheight="47" tilecount="25" columns="5">
+ <image source="tile_map.png" width="235" height="235"/>
+</tileset>
